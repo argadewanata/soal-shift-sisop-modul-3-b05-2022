7e296b8d598d9ad48e514d0560076a29acf187c1
Sidrotul Munawaroh <sidrotulmunawaroh6@gmail.com>
2022-04-09 13:53:50 +0000

    Upload New File

---
+++ soal2/server.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+int main ()
+{
+
+
+    return 0;
+}
