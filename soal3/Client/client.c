e08250e63401be6a54192b6cc31a163ff97b08ef
Miawmiw <fahmimuhazir.205025@mhs.its.ac.id>
2022-04-17 04:18:16 +0000

    Update soal3/Client/client.c

--- soal3/Client/client.c
+++ soal3/Client/client.c
@@ -5,16 +5,16 @@
 #include <arpa/inet.h>
 #define SIZE 1024
 
-void sFile(FILE *fp, int sockFd) {
+void sFile(FILE *fp, int socketFd) {
     int n;
     char data[SIZE] = {0};
 
     while (fgets(data, SIZE, fp) != NULL) {
-        if (send(sockFd, data, sizeof(data), 0) == -1) {
-            mError("~ Error in sending file.");
+        if (send(socketFd, data, sizeof(data), 0) == -1) {
+            perror("~Error in sending file.");
             exit(1);
         }
-        bZero(data, SIZE);
+        bzero(data, SIZE);
     }
 }
 
@@ -23,46 +23,46 @@ int main() {
     int port = 8080;
     int e;
 
-    int sockFd;
+    int socketFd;
     struct sockaddr_in serverAdd;
     FILE *fp;
     char *fName = "hartakarun.zip";
 
-    sockFd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockFd < 0) {
-        mError("~ Error in socket");
+    socketFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFd < 0) {
+        perror("~Error in socket");
         exit(1);
     }
-    printf("~ Server socket created successfull\n");
+    printf("~Server socket created successfully.\n");
 
     serverAdd.sin_port = port;
     serverAdd.sin_addr.s_addr = inet_addr(ip);
     serverAdd.sin_family = AF_INET;
 
 
-    e = connect(sockFd, (struct sockaddr *)&serverAdd, sizeof(serverAdd));
+    e = connect(socketFd, (struct sockaddr *)&serverAdd, sizeof(serverAdd));
     if (e == -1) {
-        mError("~ Error in socket");
+        perror("~Error in socket");
         exit(1);
     }
-    printf("~ Connected to Server\n");
+    printf("~Connected to Server.\n");
 
-    char command[100];
-    scanf("%s", command);
-    if (strcmp(command, "send hartakarun.zip") == 0) {
+    char comm[100];
+    scanf("%s", comm);
+    if (strcmp(comm, "send hartakarun.zip") == 0) {
         // scanf("%s", fName);
 
         fp = fopen(fName, "r");
         if (fp == NULL) {
-            mError("~ Error in reading file.");
+            perror("~Error in reading file.");
             exit(1);
         }
 
-        sFile(fp, sockFd);
-        printf("~ File data sent successfully.\n");
+        sFile(fp, socketFd);
+        printf("~File data sent successfully.\n");
 
-        printf("~ Closing the connection.\n");
-        close(sockFd);
+        printf("~Closing the connection.\n");
+        close(socketFd);
     }
     return 0;
 }
