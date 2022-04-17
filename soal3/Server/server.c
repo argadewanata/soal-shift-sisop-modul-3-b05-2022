0a5b107f3caa15b942f96313dfc5d7ad1b3afc59
Miawmiw <fahmimuhazir.205025@mhs.its.ac.id>
2022-04-17 04:17:34 +0000

    Update soal3/Server/server.c

--- soal3/Server/server.c
+++ soal3/Server/server.c
@@ -1,11 +1,68 @@
 #include <stdio.h>
-#include <sys/socket.h>
 #include <stdlib.h>
-#include <netinet/in.h>
 #include <string.h>
-#include <unistd.h>
 #include <arpa/inet.h>
+#define SIZE 1024
 
-int main(){
-    
+void wFile(int socketFd) { 
+    int n;
+    FILE *fp;
+    char *fName = "hartakarun.zip";
+    char buffer[SIZE];
+
+    fp = fopen(fName, "w");
+    while (1) {
+        n = recv(socketFd, buffer, SIZE, 0);
+        if (n <= 0) {
+            break;
+            return;
+        }
+        fprintf(fp, "%s", buffer);
+        bzero(buffer, SIZE);
+    }
+    return;
+}
+
+int main() {
+    char *ip = "127.0.0.1";
+    int port = 8080;
+    int e;
+
+    int socketFd, new_sock;
+    struct sockaddr_in serverAdd, new_addr;
+    socklen_t addr_size;
+    char buffer[SIZE];
+
+    socketFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFd < 0) {
+        perror("~Error in socket");
+        exit(1);
+    }
+    printf("~Server socket created successfully.\n");
+
+    serverAdd.sin_family = AF_INET;
+    serverAdd.sin_port = port;
+    serverAdd.sin_addr.s_addr = inet_addr(ip);
+
+    e = bind(socketFd, (struct sockaddr *)&serverAdd, sizeof(serverAdd));
+    if (e < 0) {
+        perror("~Error in bind");
+        exit(1);
+    }
+    printf("~Binding successfull.\n");
+
+    if (listen(socketFd, 10) == 0) {
+        printf("~Listening....\n");
+    }
+    else{
+        perror("~Error in listening");
+        exit(1);
+    }
+
+    addr_size = sizeof(new_addr);
+    new_sock = accept(socketFd, (struct sockaddr *)&new_addr, &addr_size);
+    wFile(new_sock);
+    printf("~Data written in the file successfully.\n");
+
+    return 0;
 }
